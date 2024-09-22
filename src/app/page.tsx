0332fdbdc496bb13bbdb0e646e2f94b13a1fba5b
JacobKustra <jacobkustra@gmail.com>
2024-09-22 12:52:33 -0400

    Testing Deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Hi Emily, this is Jake!</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
