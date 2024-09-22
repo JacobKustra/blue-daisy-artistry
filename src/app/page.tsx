f9ae63d162a6daa33714e53eeaa00a830dfc5f1c
JacobKustra <jacobkustra@gmail.com>
2024-09-22 14:12:02 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Hi Emily, this is Jake!! Testing here</li>
+          <li>Testing CI/CD - currently happening!</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
